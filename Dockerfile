2cb1840c859b6d24b5e463fb25629c228c7fc85b
Juan Enciso <juan.enciso@gmail.com>
2018-09-05 16:36:11 -0300

    Add files

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM tutum/lamp
+
+MAINTAINER Juan Enciso <juan.enciso@unicred.com.br>
+
+WORKDIR /var/www
+
+COPY src/ /var/www
---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CLIENT_ID", "776cglyjysa0vb");
+define("CLIENT_SECRET", "TTFvIyKQh7LywZ3T");
+define("REDIRECT_URI", "http://localhost/index.php");
+define("SCOPE", 'r_basicprofile r_emailaddress' );
+
+?>
