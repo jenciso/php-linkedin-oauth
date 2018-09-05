89461553f98d486b9a886817281b15fbb34a961a
Juan Enciso <juan.enciso@gmail.com>
2018-09-05 17:32:49 -0300

    Add files

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("CLIENT_ID", "System.getenv('CLIENT_ID')");
-define("CLIENT_SECRET", "System.getenv('CLIENT_SECRET')");
-define("REDIRECT_URI", "System.getenv('REDIRECT_URI')");
+define("CLIENT_ID", getenv('CLIENT_ID'));
+define("CLIENT_SECRET", getenv('CLIENT_SECRET'));
+define("REDIRECT_URI", getenv('REDIRECT_URI'));
 define("SCOPE", 'r_basicprofile r_emailaddress' );
 
 ?>
